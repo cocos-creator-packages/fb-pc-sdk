4dcb260218d2fb6be94fb356386aabdf721a3036
wuzhiming <wuzhiming.chukong@gmail.com>
2018-11-26 16:38:10 +0800

    add pc sdk support for fb pc games sdk

---
+++ libs/js/fb-pc-games-sdk.js
@@ -0,0 +1,40 @@
+window.fb = window.fb || {};
+
+class FBPCGamesSDK {
+  constructor() {
+    this.appId = null;
+    if (!jsb || !jsb.FacebookPCGamesSDK) {
+      cc.warn('FacebookPCGamesSDK is not defined');
+      return;
+    }
+    this.sdkInstance = new jsb.FacebookPCGamesSDK();
+  }
+
+  init(appId) {
+    this.appId = appId;
+    if (!this.sdkInstance) {
+      cc.warn('FacebookPCGamesSDK is not found');
+      return;
+    }
+    this.sdkInstance.init(appId);
+  }
+
+  login() {
+    if (this.appId) {
+      cc.warn('please call init first');
+      return;
+    }
+
+    return this.sdkInstance.login();
+  }
+
+  pushEvent(eventId, detail) {
+    if (this.appId) {
+      cc.warn('please call init first');
+      return;
+    }
+    this.sdkInstance.pushEvent(eventId, detail);
+  }
+}
+
+fb.pcGamesSDK = fb.pcGamesSDK || new FBPCGamesSDK();
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 const Path = require('fire-path');
 const Fs = require('fire-fs');
+const {base} = Editor.require('app://editor/core/native-packer');
 
 /**
  * 拷贝 pc sdk 的相关文件到proj.win32目录
@@ -10,31 +11,41 @@ const Fs = require('fire-fs');
  */
 async function handleEvent(options, cb) {
     let config = Editor._projectProfile.data['facebook'];
+    let error_tips = null;
+    do {
+        if (!config || !config.enable || !config.pcsdk.enable) {
+            break;
+        }
 
-    if (!config || !config.enable || !config['pc-sdk'].enable) {
-        cb && cb();
-        return;
-    }
+        if (!options.actualPlatform.toLowerCase() === "win32") {
+            break;
+        }
 
-    if (options.actualPlatform.toLowerCase() === "win32") {
-        cb && cb();
-        return;
-    }
+        let libPath = Editor.url('packages://fb-pc-sdk/libs/facebooksdk');
+        if (!Fs.existsSync(libPath)) {
+            error_tips = 'facebook sdk library not found';
+            break;
+        }
+        let nativePacker = new base(options);
 
-    let libPath = Editor.url('packages://fb-pc-sdk/facebooksdk');
-    if (!Fs.existsSync(libPath)) {
-        cb('facebook sdk library not found');
-        return;
-    }
+        //拷贝js文件
+        let jsLibPath = Editor.url('packages://fb-pc-sdk/libs/js');
+        let destJsPath = Path.join(options.dest, 'src');
+        Fs.copySync(jsLibPath, destJsPath);
 
-    let destPath = Path.join(options.dest, 'frameworks/runtime-src/proj.win32');
-    if (!Fs.existsSync(destPath)) {
-        cb(`${destPath} not found`);
-        return;
-    }
+        nativePacker.addRequireToMainJs('src/fb-pc-games-sdk.js');
 
-    Fs.copySync(libPath, destPath);
-    cb && cb();
+        //拷贝facebook SDK
+        let srcFbPath = Editor.url('packages://fb-pc-sdk/libs/facebooksdk');
+        let destFbPath = Path.join(options.dest, 'frameworks/runtime-src/proj.win32/facebooksdk');
+        nativePacker.ensureFile(srcFbPath, destFbPath);
+
+        //拷贝facebook sdk的依赖文件
+        let srcSDKPath = Editor.url('packages://fb-pc-sdk/libs/facebooksdk/cpprestsdk');
+        let destSDKPath = Path.join(options.dest, 'frameworks/runtime-src/proj.win32/facebooksdk/cpprestsdk');
+        nativePacker.ensureFile(srcSDKPath, destSDKPath);
+    } while (false);
+    cb && cb(error_tips);
 }
 
 module.exports = {
